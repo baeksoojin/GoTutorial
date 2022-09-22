5ce46f7d2e53b7273cc32d5856526b8e2df8ec87
sujin-create <qsoojinq@naver.com>
2022-09-23 02:34:02 +0900

    ADD : QueryRange

--- project/pkg/client/prometheus.go
+++ project/pkg/client/prometheus.go
@@ -1,1 +1,42 @@
 package client
+
+import (
+	"context"
+	"fmt"
+	"github.com/prometheus/client_golang/api"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
+	"os"
+	"time"
+)
+
+func GetData() model.Value {
+
+	fmt.Println("--------")
+	client, err := api.NewClient(api.Config{
+		Address: "http://demo.robustperception.io:9090",
+	})
+	if err != nil {
+		fmt.Printf("Error creating client: %v\n", err)
+		os.Exit(1)
+	}
+
+	v1api := v1.NewAPI(client)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	r := v1.Range{
+		Start: time.Now().Add(-time.Hour),
+		End:   time.Now(),
+		Step:  time.Minute,
+	}
+	result, warnings, err := v1api.QueryRange(ctx, `avg by (instance) (rate(node_cpu_seconds_total{job="node",mode="idle"}[5m]))*100`, r, v1.WithTimeout(5*time.Second))
+	if err != nil {
+		fmt.Printf("Error querying Prometheus: %v\n", err)
+		os.Exit(1)
+	}
+	if len(warnings) > 0 {
+		fmt.Printf("Warnings: %v\n", warnings)
+	}
+	fmt.Printf("Result:\n%v\n", result)
+	return result
+}
