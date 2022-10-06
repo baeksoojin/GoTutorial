b77d598125d28a74e33cc1fbcfab235ebbdf80ab
sujin-create <qsoojinq@naver.com>
2022-10-06 15:42:50 +0900

    ADD : flag test using another receiver

--- example/cmd/main.go
+++ example/cmd/main.go
@@ -35,6 +35,12 @@ func main() {
 		t := tutorial.NewSet(flags)
 		fmt.Println("get value", t.GetValue())
 	}()
+	go func() { //test flags -> 2개 test
+		wg.Add(1) //waitgroup이 가진 값을 arg만큼 증가
+		defer wg.Done()
+		t2 := tutorial.NewSet2(flags)
+		fmt.Println("get value2", t2.GetValue())
+	}()
 	tutorial.Say("world")
 
 	wg.Wait() //wg.Done으로 WaitGroup 객체가 모두 종료될때까지 기다린다.
---
+++ example/pkg/tutorial/flag2.go
@@ -0,0 +1,33 @@
+package tutorial
+
+import (
+	"github.com/spf13/pflag"
+	"os"
+)
+
+type Test2 struct {
+	Testvalue string
+	flags     *pflag.FlagSet
+}
+
+func NewSet2(flags *pflag.FlagSet) *Test2 {
+	return &Test2{
+		flags: flags,
+	}
+}
+
+func (t2 *Test2) Parse() error {
+	err := t2.flags.Parse(os.Args)
+	return err
+}
+
+func (t2 *Test2) GetValue() string {
+
+	t2.AddFlags()
+	t2.Parse()
+	return t2.Testvalue
+}
+
+func (t2 *Test2) AddFlags() {
+	t2.flags.StringVar(&t2.Testvalue, "testvalue2", "test2", "test value2")
+}
